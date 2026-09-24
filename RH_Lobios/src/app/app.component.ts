b62042b0a6ba9d2bd5f52dea4cc8eb006aaec8b9
zuilha <fontenelewillian@gmail.com>
2026-09-23 21:44:49 -0300

    Formulário --> json

--- RH_Lobios/src/app/app.component.ts
+++ RH_Lobios/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'RH_Lobios';
+  meuFormulario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.meuFormulario = this.fb.group({
+      nome: ['', Validators.required],
+      senha: ['', Validators.required ],
+      mensagem: []
+    });
+  }
 }
+
