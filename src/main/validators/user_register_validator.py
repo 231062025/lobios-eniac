e6bf0b8e86fde961d9689a18da7fc0133182bb02
Davi Guerreiro <231062025@eniac.edu.br>
2026-09-25 17:22:22 -0300

    Update user_register_validator.py

--- src/main/validators/user_register_validator.py
+++ src/main/validators/user_register_validator.py
@@ -3,22 +3,17 @@ from uuid import UUID
 from typing import Optional,Literal
 
 class UserRegisterValidator(BaseModel):
-    username: str = Field(..., min_length=3, max_length=50)
     nome: str = Field(..., min_length=2)
     email: EmailStr
     
 class UserUpdateValidator(BaseModel):
-    username: Optional[str] = None
     nome: Optional[str] = None
     email: Optional[EmailStr] = None
-    cargo: Optional[str] = None
-    id: Optional[UUID] = None
     tipo_perfil: Optional[Literal["colaborador", "gestor", "rh"]] = None
     cargo_id: Optional[str] = None
     setor_id: Optional[str] = None
     gestor_id: Optional[str] = None
     data_admissao: Optional[str] = None
-    criado_em: Optional[str] = None
     
 class UserResponse(BaseModel):
     id: UUID                   
