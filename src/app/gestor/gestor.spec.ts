3b271626826de4037ad7502e543d5ad7d91adc13
217872025@eniac.edu.br <>
2026-09-24 21:48:09 -0300

    TesteV5

--- src/app/gestor/gestor.spec.ts
+++ src/app/gestor/gestor.spec.ts
@@ -1,18 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { GestorComponent } from './gestor.component'; // Verifique o nome correto do arquivo e classe
 
-import { Gestor } from './gestor';
-
-describe('Gestor', () => {
-  let component: Gestor;
-  let fixture: ComponentFixture<Gestor>;
+describe('GestorComponent', () => {
+  let component: GestorComponent;
+  let fixture: ComponentFixture<GestorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Gestor]
+      imports: [GestorComponent], // Se for um Standalone Component
+      providers: [
+        provideRouter([]) // Fornece o suporte a rotas necessárias pelo routerLink/router-outlet
+      ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Gestor);
+    fixture = TestBed.createComponent(GestorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/gestor/gestor.ts
+++ src/app/gestor/gestor.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { AppComponent } from '../app.component';
 //FODA-SE easter egg
 @Component({
   selector: 'app-gestor',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, AppComponent],
   templateUrl: './gestor.html',
   styleUrl: './gestor.css'
 })
